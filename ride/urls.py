6948ce0e54efb4d12ea365f1115cad6e5dd7c6bc
a-delune <adrianlara.jpg@gmail.com>
2026-05-08 12:59:18 -0600

    changes in html/2

--- ride/urls.py
+++ ride/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('api/', views.index, name='index'),
     path('request/', views.request_ride, name='request_ride'),
     path('confirmation/<int:pk>/', views.confirmation, name='confirmation'),
 ]
